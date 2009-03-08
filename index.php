c99e40e807257e10a3ac8644a6106cafe70b3771
Tyler Hall <tylerhall@gmail.com>
2009-03-08 02:26:13 +0000

    Page width and layout switchers are working.

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
  "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-    <!-- TODO: Switch Theme box style -->
-    <!-- TODO: Notice box (block without header essentially) -->
     <title>YUI App Theme</title>
     <link rel="stylesheet" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
     <link rel="stylesheet" href="css/yuiapp.css" type="text/css">
+
+	<script src="http://yui.yahooapis.com/3.0.0pr2/build/yui/yui-min.js" type="text/javascript"></script>
+	<script type="text/javascript" charset="utf-8">
+		YUI().use('node', function(Y) {
+		     function init() {
+				Y.all('#page-width-switcher a').on('click', function(e) {
+					Y.all('#page-width-switcher a.highlight').removeClass('highlight');
+					e.target.addClass('highlight');
+					document.body.id = e.target.get('title');
+					e.halt();
+				});
+
+				Y.all('#page-layout-switcher a').on('click', function(e) {
+					Y.all('#page-layout-switcher a.highlight').removeClass('highlight');
+					e.target.addClass('highlight');
+					doc = Y.get('body div:first');
+					doc.replaceClass(doc.get('className'), e.target.get('title'));
+					e.halt();
+				});
+		     }
+		     Y.on("domready", init);
+		});
+	</script>	
 </head>
 <body>
     <div id="doc3" class="yui-t6">
@@ -294,21 +315,21 @@
 					</div>
 					<div class="bd">
 						<h3>Page Width</h3>
-						<ul class="biglist">
+						<ul id="page-width-switcher" class="biglist">
 							<li><a href="#" title="doc">750px centered</a></li>
 							<li><a href="#" title="doc2">950px centered</a></li>
-							<li><a href="#" title="doc3">100% fluid</a></li>
+							<li><a href="#" title="doc3" class="highlight">100% fluid</a></li>
 							<li><a href="#" title="doc4">974px fluid</a></li>
 						</ul>
 
 						<h3>Layout</h3>
-						<ul class="biglist">
+						<ul id="page-layout-switcher" class="biglist">
 							<li><a href="#" title="yui-t1">Left sidebar, 160px</a></li>
 							<li><a href="#" title="yui-t2">Left sidebar, 180px</a></li>
 							<li><a href="#" title="yui-t3">Left sidebar, 300px</a></li>
 							<li><a href="#" title="yui-t4">Right sidebar, 180px</a></li>
 							<li><a href="#" title="yui-t5">Right sidebar, 240px</a></li>
-							<li><a href="#" title="yui-t6">Right sidebar, 300px</a></li>
+							<li><a href="#" title="yui-t6" class="highlight">Right sidebar, 300px</a></li>
 						</ul>
 					</div>
 				</div>
